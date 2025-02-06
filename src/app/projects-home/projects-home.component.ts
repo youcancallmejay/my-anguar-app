749137a5a9a4b7465c1b95548e7c6390a52d6fab
youcancallmejay <148828634+youcancallmejay@users.noreply.github.com>
2025-02-05 22:40:59 -0600

    lesson 9 complete

--- src/app/projects-home/projects-home.component.ts
+++ src/app/projects-home/projects-home.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-projects-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './projects-home.component.html',
-  styleUrl: './projects-home.component.css'
+@Component({ 
+ standalone: true, 
+ imports: [], 
+ selector: 'app-projects-home', 
+ templateUrl: './projects-home.component.html', 
+ styleUrls: ['./projects-home.component.css']
 })
-export class ProjectsHomeComponent {
 
+export class ProjectsHomeComponent implements OnInit { 
+  firstName: string ="William"; 
+  lastName: string = "Wallace"; 
+  phoneNumber: string = "(201) 748-6000";
+
+  setProperty(event: Event){ 
+   let target = event.target as HTMLInputElement 
+   let property: string = target.id 
+   switch(property){ 
+     case "firstNameInput": 
+       this.firstName = target.value; 
+       break; 
+     case "lastNameInput": 
+       this.lastName = target.value; 
+       break; 
+     case "phoneNumberInput": 
+       this.phoneNumber = target.value; 
+       break; 
+     default: 
+        console.warn("No matching ID found!") 
+       break; 
+    } 
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {  }
 }
